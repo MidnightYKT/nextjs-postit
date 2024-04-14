c342f4fe9202b22953daebcda04ab68d0eae943a
MidnightYKT <nomiama_ykt@mail.ru>
2024-04-14 16:25:12 -0700

    added component AddPost

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import CreatePost from "./components/AddPost";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-lg ">Hello next</h1>
+      <CreatePost />
     </main>
   );
 }

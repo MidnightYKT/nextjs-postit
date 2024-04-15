818893ce688d7f9179b20f48b12f50d0c7134b2e
MidnightYKT <nomiama_ykt@mail.ru>
2024-04-14 21:43:53 -0700

    added API get for posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 "use client";
+import axios from "axios";
 import CreatePost from "./components/AddPost";
+import { useQuery } from "@tanstack/react-query";
+
+//Fetch all posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPost");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["Posts"],
+  });
+
+  if (error) return error;
+  if (isLoading) return "Loading";
+
   return (
     <main>
-      <h1 className="text-lg ">Hello next</h1>
       <CreatePost />
     </main>
   );
